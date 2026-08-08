7d1e73974e97973c3b8bdb1df30eb09aaad2c576
ariamathc4 <299699748+ariamathc4@users.noreply.github.com>
2026-08-08 18:58:50 -0400

    remove home h1

--- app/pages/home.tsx
+++ app/pages/home.tsx
@@ -3,9 +3,6 @@ export default function Home() {
         <div className="mt-3 flex justify-center">
             <div className="relative h-[750px] w-full max-w-[1400px] overflow-hidden rounded-[28px] border border-white/[0.14] bg-white/[0.055] backdrop-blur-2xl backdrop-saturate-150">
                 <div className="relative p-8">
-                    <h1 className="mb-6 text-3xl font-bold text-white underline">
-                        Home
-                    </h1>
                 </div>
             </div>
         </div>
